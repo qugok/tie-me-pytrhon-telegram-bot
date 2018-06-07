98aa4c9b1fec15101f8d647b65937729a2645d2f
qugok <iesekela@mail.ru>
2018-06-07 12:13:52 +0300

    chat_id

--- MyBot.py
+++ MyBot.py
@@ -36,7 +36,8 @@ class MyBot:
     def handle_message(self, bot: telegram.Bot, update: telegram.Update):
         # print(update)
         user = update.message['chat']['username']
-        print(user)
+        if user not in admin_usernames or other_users:
+            print(user)
         chat_id = int(update.message.chat_id)
         if user in admin_usernames and user in other_users:
             other_users.remove(user)
